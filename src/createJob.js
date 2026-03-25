9112fe06ccf19a16567cbea52c6faa705bfb96a2
Louis A Fratesi <lfratesi3@gatech.edu>
2026-03-25 13:03:33 -0500

    updates to createJob lambda

--- src/createJob.js
+++ src/createJob.js
@@ -1,8 +1,30 @@
+// const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+// const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
+
+const AWS = require("aws-sdk");
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = async (event) => {
+  const job = JSON.parse(event.body);
+  const jobId = Date.now().toString();
+
+  const newJob = {
+    jobId: jobId,
+    title: job.title,
+    company: job.company
+  };
+  const params = {
+    TableName: "jobs",
+    Item: newJob
+  };
+
+  await dynamodb.put(params).promise();
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "createJob Lambda is working!"
+      message: "createJob Lambda is working!",
+      job: job
     })
   }
 }
