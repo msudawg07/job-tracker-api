0446007257ebae693f2ab90400c1d30601a55f48
Louis A Fratesi <lfratesi3@gatech.edu>
2026-03-27 08:25:01 -0500

    update createJob

--- src/createJob.js
+++ src/createJob.js
@@ -5,25 +5,72 @@ const AWS = require("aws-sdk");
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-  const body = JSON.parse(event.body);
+
+  if(!event.body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Request body is empty."
+      })
+    }
+  }
+
   const jobId = Date.now().toString();
+  let body;
+
+  try {
+    body = JSON.parse(event.body);
+  }
+  catch (error){
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: `Invalid JSON in request body. - ${error.message}`
+      })
+    }
+  }
+
+  // check for required fields
+  if(!body.title || !body.company) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "title and company are required."
+      })
+    }
+  }
 
   const newJob = {
     jobId: jobId,
     title: body.title,
-    company: body.company
+    company: body.company,
+    salary: body.salary,
+    city: body.city,
+    state: body.state,
+    applied: body.applied,
+    status: body.status
   };
   const params = {
     TableName: "jobs",
     Item: newJob
   };
 
-  await dynamodb.put(params).promise();
+  try {
+    await dynamodb.put(params).promise();
+  }
+  catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: `Server error - ${error.message}`
+      })
+    }
+  }
 
   return {
-    statusCode: 200,
+    statusCode: 201,
     body: JSON.stringify({
-      message: "new job added successfully",
+      message: "New job added successfully",
       job: newJob
     })
   }
